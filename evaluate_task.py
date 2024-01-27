63c851a041022eb5dbe0a748c53e08b21cbf4b0b
simon <s.bresciani026@studenti.unibs.it>
2024-01-27 12:10:00 +0100

    Scelta multipla funziona ma con sys message contentente le scelte, ora provo a fare una concatenazione di stringhe per aggiungere le scelte in coda al sys message prese direttamente dal file json

--- evaluate_task.py
+++ evaluate_task.py
@@ -1,35 +1,55 @@
 import os
 import json
+import random
 import time
 import pandas as pd
 from get_completion import get_completion_zero_shot
 
+
+N_EXAMPLES = 5
+
+
+def shrink_examples(data):
+    #shrinks the examples to n examples randomly selected with no repetitions
+        examples = data.get("examples", [])
+        if len(examples) > N_EXAMPLES:
+            examples = random.sample(examples, N_EXAMPLES) 
+        return examples
+
+                  
+
 def evaluate_multiple_choice_task(json_string, system_message, model):    
     
-    df = pd.DataFrame(columns=['prompt','expected', 'answer', 'correct'])
+        df = pd.DataFrame(columns=['prompt','expected', 'answer', 'correct'])
 
 
-    with open(json_string, 'r') as file:
-        data = json.load(file)
+        with open(json_string, 'r') as file:
+                data = json.load(file)
 
-    for example in data.get("examples", []):
-        prompt = example.get("input")
-        expected = [k for k, v in example['target_scores'].items() if v == 1][0]
-        answer = None
-        try:
-                answer = get_completion_zero_shot(system_message, prompt, model)
-        except Exception as e:
-                print("Error in example: ", example)
-                print("Exeption: ", e.args[0])
-                #mi fermo per 30 secondi
-                time.sleep(30)
-                #ritento la richiesta
-                answer = get_completion_zero_shot(system_message, prompt, model)
-        finally:                
-                if answer is not None :
-                      correct = answer[1:] == expected
-                else :
-                      correct = False       
-                print(df)
+        examples_selected = shrink_examples(data)
+
+        for example in examples_selected:
+                prompt = example.get("input")
+                expected = [k for k, v in example['target_scores'].items() if v == 1][0]
+                answer = None
+                try:
+                        answer = get_completion_zero_shot(system_message, prompt, model)
+                except Exception as e:
+                        print("Error in example: ", example)
+                        print("Exeption: ", e.args[0])
+                        #mi fermo per 30 secondi
+                        time.sleep(30)
+                        #ritento la richiesta
+                        answer = get_completion_zero_shot(system_message, prompt, model)
+                finally:                
+                        if answer is not None :
+                                #check on the first word of the answer if it contains the expected answer
+                                correct = answer.split()[0].__contains__(expected) 
+                        else :
+                                correct = False       
                 df.loc[len(df.index)] = [prompt, expected, answer, correct]
-    return df
+        df.to_csv("results/anachronisms_gpt3_5.csv", index=True)
+        
+        accuracy = df['correct'].mean()
+        return accuracy * 100
+        
--- get_completion.py
+++ get_completion.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv(dotenv_path=".env")
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY_3.5"), organization = "org-FwcUlxzfJx5sO1h5F3lxshSC")
 
-def get_completion_zero_shot(system, prompt, model): 
+def get_completion_zero_shot(system, prompt, model):
+    #if the model is the 3.5 turbo, I use my personal free api key, otherwise I use the one of the organization
+    if(model == "gpt-3.5-turbo-1106"):
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY_3.5"))
+    else:
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY_3.5"), organization = "org-FwcUlxzfJx5sO1h5F3lxshSC")
+    
     response = client.chat.completions.create(
         model=model,
         messages=[{"role": "system", "content": system },
