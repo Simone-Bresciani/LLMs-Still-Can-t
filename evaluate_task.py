189066262eca3854d17c4ccbaae6d1371f72318a
simon <s.bresciani026@studenti.unibs.it>
2024-01-27 20:24:19 +0100

    implementata evaluate free response.

--- evaluate_task.py
+++ evaluate_task.py
@@ -9,6 +9,8 @@ from get_completion import get_completion_zero_shot
 
 N_EXAMPLES = 50
 MULTIPLE_CHOICE_SUFFIX = "First answer repeating the answer you choose, in the second line explain your answer in 20 words. Choices:"
+FREE_RESPONSE_SUFFIX = "In the first line you answer just the result, in the second line explain your answer in 20 words."
+
 
 def shrink_examples(data, n):
         examples = data.get("examples", [])
@@ -23,11 +25,43 @@ def built_system_message(system_message, example):
             system_message = system_message + "-" + choice + "\n"
         return system_message 
                   
+def evaluate_free_response_task(json_string, system_message, model):
+        df = pd.DataFrame(columns=['prompt','expected', 'answer', 'explanation', 'correct'])
+        with open(json_string, 'r') as file:
+                data = json.load(file)
+        
+        examples_selected = shrink_examples(data, N_EXAMPLES)
+        for example in examples_selected:
+                prompt = example.get("input")
+                expected = example.get("target")
+                response = None
+                answer = None
+                explanation = None
+                new_system_message = system_message + " " + FREE_RESPONSE_SUFFIX
+
+                try:
+                        response = get_completion_zero_shot(new_system_message, prompt, model)
+                except Exception as e:
+                        print("Completion Exception in example: ", example)
+                        time.sleep(60)
+                        response = get_completion_zero_shot(new_system_message, prompt, model)
+                finally:                
+                        if response is not None :
+                                re.sub("\n+", "\n", response)
+                                answer, explanation = response.split("\n", 1)
+                                answer = answer.replace(' ', '')
+                                correct = answer.__contains__(expected) 
+                        else :
+                                correct = False       
+                df.loc[len(df.index)] = [prompt, expected, answer, explanation,  correct]
+        
+        result_path = f"results/{json_string.split('/')[1].split('.')[0]}_results.json"       
+        df.to_json(result_path, index=True)
+        accuracy = df['correct'].mean()
+        return round(accuracy, 2)       
 
 def evaluate_multiple_choice_task(json_string, system_message, model):   
-
         df = pd.DataFrame(columns=['prompt','expected', 'answer', 'explanation', 'correct'])
-
         with open(json_string, 'r') as file:
                 data = json.load(file)
 
@@ -61,8 +95,7 @@ def evaluate_multiple_choice_task(json_string, system_message, model):
                 df.loc[len(df.index)] = [prompt, expected, answer, explanation,  correct]
 
         result_path = f"results/{json_string.split('/')[1].split('.')[0]}_results.json"       
-        df.to_json(result_path, index=True)
-        
+        df.to_json(result_path, index=True)     
         accuracy = df['correct'].mean()
         return round(accuracy, 2)
         
