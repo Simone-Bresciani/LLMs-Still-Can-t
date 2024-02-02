e9af8d278f8ef23547992ad29ce78047d1b6c2dc
simon <s.bresciani026@studenti.unibs.it>
2024-02-02 10:47:40 +0100

    pre correzione

--- evaluate_task.py
+++ evaluate_task.py
@@ -8,24 +8,23 @@ from get_completion import get_completion_zero_shot
 
 
 
-N_EXAMPLES = 2
+N_EXAMPLES = 10
 MULTIPLE_CHOICE_SUFFIX = "First answer repeating the answer you choose, in the second line explain your answer in 20 words. Choices:"
 FREE_RESPONSE_SUFFIX = "In the first line you answer just the result, in the second line explain your answer in 20 words."
 
 class Task:
-        def __init__(self, type, json_string, system_message):
+        def __init__(self, type, json_string, system_prompt):
                 self.type = type
                 self.json_string = json_string
-                self.system_message = system_message
+                self.system_prompt = system_prompt
 
         def evaluate(self, model):
                 if self.type == "multiple_choice":
-                        return evaluate_multiple_choice_task(self.json_string, self.system_message, model)
+                        return evaluate_multiple_choice_task(self.json_string, self.system_prompt, model)
                 elif self.type == "free_response":
-                        return evaluate_free_response_task(self.json_string, self.system_message, model)
+                        return evaluate_free_response_task(self.json_string, self.system_prompt, model)
                 else:
-                        raise Exception("Task not supported, check the type of the task.")
-                
+                        raise Exception("Task not supported, check the type of the task.")                
 
 def shrink_examples(data, n):
         examples = data.get("examples", [])
@@ -34,13 +33,13 @@ def shrink_examples(data, n):
             examples = random.sample(examples, n) 
         return examples
 
-def built_system_message(system_message, example):
-        system_message = system_message + " " + MULTIPLE_CHOICE_SUFFIX
+def built_system_message(system_prompt, example):
+        system_prompt = system_prompt + " " + MULTIPLE_CHOICE_SUFFIX
         for choice in example.get("target_scores", {}).keys():
-            system_message = system_message + "-" + choice + "\n"
-        return system_message 
+            system_prompt = system_prompt + "-" + choice + "\n"
+        return system_prompt 
                   
-def evaluate_free_response_task(json_string, system_message, model):
+def evaluate_free_response_task(json_string, system_prompt, model):
         df = pd.DataFrame(columns=['prompt','expected', 'answer', 'explanation', 'correct'])
         with open(json_string, 'r') as file:
                 data = json.load(file)
@@ -52,7 +51,7 @@ def evaluate_free_response_task(json_string, system_message, model):
                 response = None
                 answer = None
                 explanation = None
-                new_system_message = system_message + " " + FREE_RESPONSE_SUFFIX
+                new_system_message = system_prompt + " " + FREE_RESPONSE_SUFFIX
 
                 try:
                         response = get_completion_zero_shot(new_system_message, prompt, model)
@@ -73,9 +72,9 @@ def evaluate_free_response_task(json_string, system_message, model):
         result_path = f"results/{json_string.split('/')[1].split('.')[0]}_results.json"       
         df.to_json(result_path, index=True)
         accuracy = df['correct'].mean()
-        return round(accuracy, 2)       
+        return round(accuracy, 2) , df      
 
-def evaluate_multiple_choice_task(json_string, system_message, model):   
+def evaluate_multiple_choice_task(json_string, system_prompt, model):   
         df = pd.DataFrame(columns=['prompt','expected', 'answer', 'explanation', 'correct'])
         with open(json_string, 'r') as file:
                 data = json.load(file)
@@ -92,7 +91,7 @@ def evaluate_multiple_choice_task(json_string, system_message, model):
                 response = None
                 answer = None
                 explanation = None
-                new_system_message = built_system_message(system_message, example)
+                new_system_message = built_system_message(system_prompt, example)
 
                 try:
                         response = get_completion_zero_shot(new_system_message, prompt, model)
@@ -112,5 +111,5 @@ def evaluate_multiple_choice_task(json_string, system_message, model):
         result_path = f"results/{json_string.split('/')[1].split('.')[0]}_results.json"       
         df.to_json(result_path, index=True)     
         accuracy = df['correct'].mean()
-        return round(accuracy, 2)
+        return round(accuracy, 2) , df
         
