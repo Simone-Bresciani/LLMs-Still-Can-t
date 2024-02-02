ff6fb62e474ff0e8e61bdbd92d80d7ec792ab400
simon <s.bresciani026@studenti.unibs.it>
2024-02-02 12:32:15 +0100

    ripristinato Main

--- shrink_dataset.py
+++ shrink_dataset.py
@@ -1,17 +1,26 @@
 import random
 import json
+import os
 
 N_EXAMPLES = 50
 MAX_SHOTS = 4
 
-def shrink_examples(json_string, (N_EXAMPLES,MAX_SHOTS) as n ): #persistenti
-        examples = json_string.get("examples", [])
-        random.shuffle(examples) # shuffle examples so the difficult ones are not always at the end
-        if len(examples) > n:
-            examples = random.sample(examples, n) 
-        return examples
+def extract_datasets(): #persistenti 
+       for filename in os.listdir('data'):
+              with open(filename, 'r') as file:
+                     data = json.load(file)
+                     examples = data.get("examples", [])
+              random.shuffle(examples) # shuffle examples so the difficult ones are not always at the end
 
+              if len(examples) > N_EXAMPLES+MAX_SHOTS:
+                     examples = random.sample(examples, N_EXAMPLES+MAX_SHOTS) 
 
+              shots = examples[:MAX_SHOTS]
+              examples = examples[MAX_SHOTS:]
+#devo fare la scrittura su file
+              with open(f'datasets/{filename}', 'w') as file:
+                     json.dump(data, file)
+              
         if(json_string.__contains__("color")):
                examples_selected = shrink_examples(data, N_EXAMPLES//4)
         else:
